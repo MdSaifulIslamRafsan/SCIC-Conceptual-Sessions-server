dd4ff43e64fe769c42a7552214ef1ca884938301
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2024-08-25 23:43:53 +0600

    add me route

--- index.js
+++ index.js
@@ -114,7 +114,11 @@ async function run() {
   }
     
 
- })
+ });
+
+ app.get('/me', async(req, res) =>{
+
+ });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
