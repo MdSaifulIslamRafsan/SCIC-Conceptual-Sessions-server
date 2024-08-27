3254dd77effa816413fe2e5ca0e5df6b17692327
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2024-08-27 17:29:26 +0600

    added verifyToken

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-const { hashPassword } = require('./middleware');
+const { hashPassword, verifyToken } = require('./middleware');
 require('dotenv').config();
 const app = express(); 
 const port = process.env.PORT || 5000;
@@ -77,7 +77,7 @@ async function run() {
    }
  })
 
- app.post("/login", async(req, res) => {
+ app.post("/login",  async(req, res) => {
   try {
     const {phoneNumber , password} = req.body;
     const userDetails = await userCollection.findOne({phoneNumber: phoneNumber})
@@ -116,7 +116,7 @@ async function run() {
 
  });
 
- app.get('/me', async(req, res) =>{
+ app.get('/me',  async(req, res) =>{
 
  });
     // Send a ping to confirm a successful connection
--- middleware.js
+++ middleware.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const hashPassword = (req, res, next) =>{
 const {password} = req.body;
     const hash = bcrypt.hashSync(password, 10);
@@ -7,5 +8,11 @@ const {password} = req.body;
 next()
 }
 
+const verifyToken =  (req, res, next) =>{
+    const token =  req.headers?.authorization.split(' ')[1];
+    const decoded = jwt.verify(token, process.env.SECRET_TOKEN);
+    console.log({decoded});
 
-module.exports= {hashPassword}
+    
+}
+module.exports= {hashPassword, verifyToken}
